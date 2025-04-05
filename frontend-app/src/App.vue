7c5163b23568dbdf43cc10fef78a7b659cf24ca7
nicoCT04 <nicoconcua@gmail.com>
2025-04-05 16:58:23 -0600

    Docker Up

---
+++ frontend-app/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const pacientes = ref([])
+
+onMounted(async () => {
+  try {
+    const res = await fetch(import.meta.env.VITE_API_URL + '/pacientes')
+    pacientes.value = await res.json()
+  } catch (error) {
+    console.error('Error al conectar con el backend:', error)
+  }
+})
+</script>
+
+<template>
+  <div style="padding: 2rem">
+    <h1>Pacientes</h1>
+    <ul>
+      <li v-for="p in pacientes" :key="p.id">{{ p.nombre }}</li>
+    </ul>
+  </div>
+</template>
+
+
