3e470678a091333d2cf7dc19eef729829c0e3809
Nesstor07 <erneram1975@gmail.com>
2025-07-18 09:21:29 -0600

    Ficha de datos Dialog

--- frontend-app/src/components/patientsDialogsComponents/patientsDataSheetDialog.vue
+++ frontend-app/src/components/patientsDialogsComponents/patientsDataSheetDialog.vue
@@ -1,26 +1,85 @@
 <template>
-     <general-dialog-modal ref="createDialog" @close-modal="handleClose" :isOpen="isOpen"
-          :dialogClass="' rounded-md bg-white px-4 py-2 text-sm font-medium text-black hover:bg-black/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75'">
-          <template #title>   
-               Si
+     <general-dialog-modal ref="createDialog" @close-modal="handleClose" :isOpen="isOpen" dialogSize="max-w-6xl">
+          <template #title>
+               <p class="text-xl">
+                    {{ $t('patients.data-sheet') }}
+               </p>
           </template>
           <template #body>
-               body
+               <div className="grid grid-cols-2 grid-rows-3 gap-4">
+                    <div className="col-span-2">
+                         <panel>
+                              <p class="font-bold">
+                                   {{ $t('patients.patients-data') }}
+                              </p>
+                              <div class="grid grid-cols-2 gap-2 mt-2">
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.first-name') }}</p>
+                                        <p>{{ firstName }}</p>
+                                   </div>
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.last-name') }}</p>
+                                        <p>{{ lastName }}</p>
+                                   </div>
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.birthdate') }}</p>
+                                        <p>{{ insurance }}</p>
+                                   </div>
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.insurance') }}</p>
+                                        <p>{{ familyPediatrician }}</p>
+                                   </div>
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.family-pediatrician') }}</p>
+                                        <p>{{ gender }}</p>
+                                   </div>
+                                   <div>
+                                        <p class="font-thin text-gray-600">{{ $t('general.sex') }}</p>
+                                        <p>{{ birthdate }}</p>
+                                   </div>
+                              </div>
+                         </panel>
+                    </div>
+                    <div className="row-start-2">
+                         <panel>
+                              2
+                         </panel>
+                    </div>
+                    <div className="row-start-2">
+                         <panel>
+                              3
+                         </panel>
+                    </div>
+                    <div className="row-start-3">
+                         <panel>
+                              4
+                         </panel>
+                    </div>
+                    <div className="row-start-3">
+                         <panel>
+                              5
+                         </panel>
+                    </div>
+               </div>
           </template>
           <template #buttons>
-       ai
+               av
           </template>
      </general-dialog-modal>
 </template>
 <script setup>
 import { usePatientsLogicStore } from '@stores/patientsLogicStore.js'
+import {usePatientsStore} from "@stores/patientsStore.js"
 import { DialogTitle } from '@headlessui/vue'
 import { useForm } from 'vee-validate'
 import { ref, defineAsyncComponent, computed } from 'vue'
 import { storeToRefs } from "pinia"
 
 import GeneralDialogModal from "@components/forms/GeneralDialogModal.vue"
+import Panel from "@components/forms/Panel.vue"
 
+const patientsStore = usePatientsStore()
+const { currentPatientSelectedData } = storeToRefs(patientsStore)
 
 const patientsLogicStore = usePatientsLogicStore()
 const { showDataSheetPatientDialog } = storeToRefs(patientsLogicStore)
@@ -42,6 +101,14 @@ const props = defineProps({
           type: Boolean,
           default: false
      },
-
 })
+
+console.log(currentPatientSelectedData.value)
+const firstName = ref(currentPatientSelectedData.value.name)
+const lastName = ref(currentPatientSelectedData.value.lastName)
+const insurance = ref(currentPatientSelectedData.value.gender)
+const gender = ref(currentPatientSelectedData.value.gender)
+const familyPediatrician = ref(currentPatientSelectedData.value.gender)
+const birthdate = ref(currentPatientSelectedData.value.birthdate)
+
 </script>
