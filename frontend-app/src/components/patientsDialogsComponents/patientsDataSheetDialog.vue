76ef951c095799a1e9b1698e563f0cba420c4b1f
Nesstor07 <erneram1975@gmail.com>
2025-07-18 15:04:55 -0600

    Comment

--- frontend-app/src/components/patientsDialogsComponents/patientsDataSheetDialog.vue
+++ frontend-app/src/components/patientsDialogsComponents/patientsDataSheetDialog.vue
@@ -102,7 +102,7 @@ const props = defineProps({
           default: false
      },
 })
-
+// Probando comentarios
 // Computed para acceder a los datos reactivamente
 const patientData = computed(() => patientsStore.currentPatientSelectedData.value)
 
