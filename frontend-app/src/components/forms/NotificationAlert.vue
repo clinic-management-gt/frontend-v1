9a93a5391f22d35551f5ee60d0b451f6e464dad9
Nesstor07 <114420481+erneram@users.noreply.github.com>
2025-08-28 22:49:09 -0600

    CLINIC-106: Validar endpoints en frotend para subir y descargar archivos de Cloudfare (#17)
    # Validar endpoints en frotend para subir y descargar archivos de
    Cloudfare
    ## Validación de subida de archivo
    
    <img width="1440" height="819" alt="image"
    src="https://github.com/user-attachments/assets/c10e1e8d-f47d-48b4-a645-5cecaa7e8750"
    />
    
    ### Como se manda en el store
    
    <img width="461" height="380" alt="image"
    src="https://github.com/user-attachments/assets/e0242507-eb11-40e7-a025-f03f7b8dbb39"
    />
    
    ---
    ## Validación de descarga de archivo
    
    <img width="1427" height="808" alt="image"
    src="https://github.com/user-attachments/assets/012ae822-0aa4-4f9f-baba-3bb9329ecc78"
    />
    
    ### Como se manda en el store
    
    <img width="504" height="524" alt="image"
    src="https://github.com/user-attachments/assets/ea3f7bdb-c41e-4131-b69d-d2a8da8e84bb"
    />
    
    ---

--- frontend-app/src/components/forms/NotificationAlert.vue
+++ frontend-app/src/components/forms/NotificationAlert.vue
@@ -1,3 +1,45 @@
+<template>
+  <Teleport to="body">
+    <div aria-live="assertive"
+    class="fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-[9999] pointer-events-none">
+      <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+        <transition v-for="(n, index) in notifications" :key="index"
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0">
+          <div v-if="n.visible"
+          :class="['pointer-events-auto cursor-default w-full max-w-sm overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5', styleFor(n.type).card]"
+          @mouseenter="notificationStore.startHovering(n.id)"
+          @mouseleave="notificationStore.stopHovering(n.id)">
+            <div class="p-4">
+              <div class="flex items-center">
+                <component :is="styleFor(n.type).icon" :class="['h-8 w-8', styleFor(n.type).iconClass]"
+                aria-hidden="true" />
+                <div class="ml-3 w-0 flex-1">
+                  <p :class="[styleFor(n.type).text.primary, 'text-sm font-medium']">
+                    {{ t(n.statusTitle) }}
+                  </p>
+                  <p v-if="n.statusMessage !== undefined" :class="[styleFor(n.type).text.second, 'text-sm']">
+                    {{ t(n.statusMessage) }}
+                  </p>
+                </div>
+                  <button type="button" @click="notificationStore.hideNotification(n.id)"
+                    class="ml-4 shrink-0 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon :class="['h-5 w-5', 'flex-none', styleFor(n.type).button]" aria-hidden="true"/>
+                </button>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
 <script setup>
 import { defineAsyncComponent } from "vue";
 import { useI18n } from "vue-i18n";
@@ -56,51 +98,3 @@ const STYLES = {
 // 2) Helper con fallback
 const styleFor = (type) => STYLES[type] ?? STYLES.error;
 </script>
-
-<template>
-  <Teleport to="body">
-    <div aria-live="assertive"
-         class="fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-[9999] pointer-events-none">
-      <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
-        <transition v-for="(n, index) in notifications" :key="index"
-          enter-active-class="transform ease-out duration-300 transition"
-          enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-          enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-          leave-active-class="transition ease-in duration-100"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0">
-          <div v-if="n.visible"
-               :class="['pointer-events-auto cursor-default w-full max-w-sm overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5', styleFor(n.type).card]"
-               @mouseenter="notificationStore.startHovering(n.id)"
-               @mouseleave="notificationStore.stopHovering(n.id)">
-            <div class="p-4">
-              <div class="flex items-start">
-                <component :is="styleFor(n.type).icon"
-                           :class="['h-8 w-8', styleFor(n.type).iconClass]"
-                           aria-hidden="true" />
-                <div class="ml-3 w-0 flex-1 pt-0.5">
-                  <p class="text-sm font-medium"
-                    :class="styleFor(n.type).text.primary">
-                    {{ t(n.statusTitle) }}
-                  </p>
-                  <p v-if="n.statusMessage !== undefined"
-                    class="mt-1 text-sm"
-                    :class="styleFor(n.type).text.second">
-                    {{ t(n.statusMessage) }}
-                  </p>
-                </div>
-                  <button type="button" @click="notificationStore.hideNotification(n.id)"
-                  class="ml-4 shrink-0 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                  <span class="sr-only">Close</span>
-                  <XMarkIcon
-                    :class="['h-5 w-5', 'flex-none', styleFor(n.type).button]"
-                    aria-hidden="true"/>
-                </button>
-              </div>
-            </div>
-          </div>
-        </transition>
-      </div>
-    </div>
-  </Teleport>
-</template>
--- frontend-app/src/components/patientsDialogsComponents/PatientsCreateFormDialog.vue
+++ frontend-app/src/components/patientsDialogsComponents/PatientsCreateFormDialog.vue
@@ -85,6 +85,8 @@ const showDialog = computed({
 
 function handleSendFiles(fileReceived) {
   rawFile.value = fileReceived
+  console.log("fileReceived", fileReceived)
+  patientsStore.uploadFile(fileReceived, "laboratorios", 1, "descripcion de prueba", 1)
   if (fileReceived) {
     generate()
     newPatientData.value.file = fileReceived
@@ -93,6 +95,20 @@ function handleSendFiles(fileReceived) {
     blobUrl.value = null
     newPatientData.value.file = null
   }
+}
+function downloadFile(patientId, type, medicalRecordID) {
+  patientsStore.downloadFile(1, "laboratorios", 1).then((blob) => {
+    const url = window.URL.createObjectURL(new Blob([blob]))
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', `ficha_datos_${patientId}.pdf`) //or any other extension
+    document.body.appendChild(link)
+    link.click()
+    link.parentNode.removeChild(link)
+    window.URL.revokeObjectURL(url)
+  }).catch((error) => {
+    console.error('Error downloading file:', error)
+  })
 }
 
 function nextPage() {
