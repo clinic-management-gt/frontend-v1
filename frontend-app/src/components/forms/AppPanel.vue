e9218a74331f85cb22fbec81a3d2cb827f119afb
Nesstor07 <114420481+erneram@users.noreply.github.com>
2025-09-30 19:51:24 -0600

    CLINIC-127: Refactorizar el model de "Historia Clinica" (#33)
    Se refactorizo diseño en el modal para la historia clinica y tambien se
    utilizaron los datos que se envian del backend para mostrarlo en el
    frontend como vacunas, archivos y entre otros.

--- frontend-app/src/components/forms/AppPanel.vue
+++ frontend-app/src/components/forms/AppPanel.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="overflow-hidden rounded-lg bg-white shadow-md px-4 py-5 sm:p-6">
+  <div :class="[panelDesign]">
     <slot></slot>
   </div>
 </template>
+<script setup>
+defineProps({
+  panelDesign: {
+    type: String,
+    default: 'overflow-hidden rounded-lg bg-white shadow-md px-4 py-5 sm:p-6'
+  },
+});
+</script>
